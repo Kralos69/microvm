7ddcbe01dc5a91e82aaa30d02e21fa60f739dd9a
Kralos69 <107302874+Kralos69@users.noreply.github.com>
2026-05-04 04:38:07 +0200

    Add level 4 tape VM

--- MicroVm/main.c
+++ MicroVm/main.c
@@ -3,96 +3,77 @@
 
 
 typedef enum  {
-    HALT            = 0x00,  // Stops execution
-    PRINT_CHAR      = 0x01,  // Print program[pc+1] as ASCII. pc += 2
-    PUSH            = 0x02,  // Push program[pc+1] onto the stack. pc += 2
-    ADD             = 0x03,  // Pop two values, push their sum.pc += 1
-    PRINT_TOP       = 0x04,  // Pop the top of the stack and print it as ASCII. pc += 1
-    JUMP            = 0x05,  // Set pc to program[pc+1]. (Don't add 2 — you're replacing pc entirely.)
-    JUMP_IF_ZERO    = 0x06,  // Peek the top of the stack. If it was 0, set pc to program[pc+1]. If it wasn't 0, just do pc += 2 and keep going.
-
+    INC                 = 0x01,  // tape[dp] += 1 (wraps: 255 + 1 = 0). pc += 1
+    DEC                 = 0x02,  // tape[dp] -= 1 (wraps: 0 - 1 = 255). pc += 1
+    RIGHT               = 0x03,  // dp += 1. pc += 1
+    LEFT                = 0x04,  // dp -= 1. pc += 1
+    PRINT               = 0x05,  // Print tape[dp] as an ASCII character. pc += 1
+    READ                = 0x06,  // tape[dp] = getchar(). pc += 1
+    JUMP_IF_ZERO        = 0x07,  // If tape[dp] == 0, set pc to program[pc+1]. Otherwise pc += 2.
+    JUMP_IF_NOT_ZERO    = 0x08,  // If tape[dp] != 0, set pc to program[pc+1]. Otherwise pc += 2.
+    HALT                = 0x00,  // Stop.
 } opcode_t;
 
-
-uint8_t program1[] = {
-    2, 65,         // 0: PUSH 65 ('A')
-    4,             // 2: PRINT_TOP
-    5, 0,          // 3: JUMP 0  (go back to the start)
-    0              // 5: HALT    (never reached)
+uint8_t program[] = {
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,   // 10 x INC
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,   // 20
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,   // 30
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,   // 40
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,   // 50
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,   // 60
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,   // 70
+    1, 1,                             // 72
+    5,                                // PRINT (prints 'H')
+    0                                 // HALT
 };
-
-//uint8_t program2[] = {
-//    2, 3,          // 0: PUSH 3         (loop counter)
-//    2, 65,         // 2: PUSH 65 ('A')
-//    4,             // 4: PRINT_TOP      (prints 'A', counter is now on top)
-//    2, 1,          // 5: PUSH 1
-//    3,             // 7: ADD            (wait — this is counter + 1, not counter - 1!)
-//};
-
-uint8_t program3_orig[] = {
-    2, 3,          //  0: PUSH 3           <- loop counter
-    2, 65,         //  2: PUSH 65 ('A')
-    4,             //  4: PRINT_TOP        <- prints 'A', counter back on top
-    2, 255,        //  5: PUSH 255
-    3,             //  7: ADD              <- counter = counter + 255 (wraps: subtract 1)
-    6, 2,          //  8: JUMP_IF_ZERO 2   <- if counter hit 0, skip to pc=2... wait
-    0              // 10: HALT
-};
-
-uint8_t program3_corrected[] = {
-    2, 3,          //  0: PUSH 3           <- loop counter
-    2, 65,         //  2: PUSH 65 ('A')
-    4,             //  4: PRINT_TOP        <- prints 'A', counter back on top
-    2, 255,        //  5: PUSH 255
-    3,             //  7: ADD              <- counter = counter + 255 (wraps: subtract 1)
-    6, 12,         //  8: JUMP_IF_ZERO 12  <- Jump to the exit.
-    5, 2,          //  10: JUMP 2          <- Jump to the start of the loop.
-    0              //  12: HALT
-};
-
 
 void run(uint8_t* program) {
 
-    uint8_t pc = 0; // Program Counter
+   uint8_t tape[256] = {0}; // all zeroes at start
+    int dp = 0;             // data pointer
+    int pc = 0;             // program counter
+
 
     opcode_t opcode = program[pc];
 
-    uint8_t stack[256];
-    uint8_t sp = 0;
-
-    while (opcode != 0x00) {
-        opcode = program[pc];
+    while (opcode != HALT) {
         switch (opcode) {
-        case PRINT_CHAR:
-            printf("%c", program[pc + 1]);
-            pc += 2;
+        case INC:
+            tape[dp]++;
+            pc++;
             break;
-        case PUSH:
-            stack[sp] = program[pc + 1];
-            sp++;
-            pc += 2;
+        case DEC:
+            tape[dp]--;
+            pc++;
             break;
-        case ADD:
-            if (sp >= 2)
-            {
-                sp--;
-                stack[sp - 1] = stack[sp - 1] + stack[sp];
-            }
-            pc += 1;
+        case RIGHT:
+            dp++;
+            pc++;
             break;
-        case PRINT_TOP:
-            if (sp > 0)
-            {
-                sp--;
-                printf("%c", stack[sp]);
-            }
-            pc += 1;
+        case LEFT:
+            dp--;
+            pc++;
             break;
-        case JUMP:
-            pc = program[pc + 1];
+        case PRINT:
+            printf("%c", tape[dp]);
+            pc++;
+            break;
+        case READ:
+            tape[dp] = getchar();
+            pc++;
             break;
         case JUMP_IF_ZERO:
-            if (sp > 0 && stack[sp - 1] == 0)
+            if (tape[dp] == 0)
+            {
+                pc = program[pc + 1];
+            }
+            else
+            {
+                pc += 2;
+            }
+            break;
+        case JUMP_IF_NOT_ZERO:
+            if (tape[dp] != 0)
             {
                 pc = program[pc + 1];
             }
@@ -114,15 +95,30 @@ void run(uint8_t* program) {
 int main() {
 
 
-    //printf("Program 1: \n");
-    //run(program1); // Infinite loop
-    //printf("\n----------------------------------\n");
-    //printf("Program 2: \n");
-    //run(program2);
-    printf("\n----------------------------------\n");
-    printf("Program 3: \n");
-    run(program3_corrected);
 
+    // Program1
+    // run(program);
+
+    // Program2: Letter 'H' with a loop
+
+
+    uint8_t program2[] = {
+        //  pc
+            1,1,1,1,1,1,1,1,            //  0-7:   INC x8 (cell 0 = 8)
+            // -- loop start at pc 8 --
+            7, 24,                      //  8-9:     JUMP_IF_ZERO 24  (exit loop)
+            3,                          // 10:     RIGHT
+            1,1,1,1,1,1,1,1,1,          // 11-19:  INC x9 (add 9 to cell 1)
+            4,                          // 20:     LEFT
+            2,                          // 21:     DEC (counter -= 1)
+            8, 8,                       // 22-23:     JUMP_IF_NOT_ZERO 8 (loop back)
+            // -- loop end --
+            3,                          // 24:     RIGHT
+            5,                          // 25:     PRINT
+            0                           // 26:     HALT
+    };
+
+    run(program2);
 
 	return 0;
 }
